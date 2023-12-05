e9f8df0d3bb3612782f684280636df06cac9eeaa
wJhonata <wjhonata1@gmail.com>
2023-12-04 21:26:18 -0300

    Home finalizada

--- pipoca-agil/src/HomePage.jsx
+++ pipoca-agil/src/HomePage.jsx
@@ -1,46 +1,70 @@
-import React from "react";
-import { IoMdPerson } from 'react-icons/io';
+import React, { useState } from "react";
+import { IoMdPerson, IoMdArrowRoundDown  } from 'react-icons/io';
 
-// function HomePage() {
-//     return(<h1>Teste1</h1>)
-    
-// }
 
-// export default HomePage
+
 function Home (){
-    return (
-      <div>
+    
+  
+  const [isActive, setIsActive] = useState(false)
+  const onClick = () => setIsActive(!isActive)
+
+
+
+  return (
+      <div className="content-main">
         <header>
             <div className="logo">
                 <img src="/assets/svg/logo.svg" alt="Logo Pipoca Ágil" />
             </div>
             <nav>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Galeria</a></li>
-                    <li><a href="">Inscreva-se</a></li>
+                <ul className="btn-header">
+                    <li><a href="" className="item-header">Home</a></li>
+                    <li><a href="" className="item-header">Galeria</a></li>
+                    <li><a href="" className="item-header">Inscreva-se</a></li>
                 </ul>
                 <div className="login-area">
-                  {/* <img src="/assets/svg/login-icon.svg" alt="Ícone de Login" /> */}
                   <IoMdPerson className="icon-person" />
                   <span></span>
-                  <a href="">Login</a>
+                  <a href="" className="btn-login">Login</a>
                 </div>
             </nav>
         </header>
+      <main>
+      <article>
+        <img src="/assets/png/bucket-popcorn.png" alt="Balde de Pipoca" />
+      </article>
 
-        {/* <section>
-          <div className="content-home">
-            <div className="content-left">
-              <img src="/assets/png/bucket-popcorn.png" alt="Balde de pipoca" />
-            </div>
-            <div className="content-right">
+      <div className="content">
+        <section className="content-right">
+          <img src="assets/png/image-ibson.png" alt="Ibson Cabral dono do pipoca ágil" />
+          <p>Seu Podcast do Mundo Ágil com <br /> muito bom humor e informação!</p>
+          
+          <div className="content-dropdown">
+          <button onClick={onClick} className="menu-btn">
+          Vem Conferir
+          <IoMdArrowRoundDown className={`icon-arrow ${isActive ? 'rotate' : ''}`}/>
+          </button>
 
+          <div className="menu-dropdown">
+            <nav className={`menu ${isActive ? 'active' : 'inactive'}`}>
+              <ul>
+                <li className="list-menu"><a href="#" className="item-menu">Assinantes</a></li>
+                <li className="list-menu"><a href="#" className="item-menu">Trilha</a></li>
+                <li className="list-menu"><a href="#" className="item-menu">Playlist</a></li>
+              </ul>
+            </nav>
             </div>
-          </div>
-        </section> */}
+            </div>
+        
+        </section>
       </div>
+      </main>
+      </div>
+
+      
     );
   };
   
   export default Home;
+ 
