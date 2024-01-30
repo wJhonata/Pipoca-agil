0b8a3ef774a7d999ba863ca921ca0d96bcae623b
devggui <devguiet@gmail.com>
2024-01-29 22:49:47 -0300

    feat: :sparkles: organização da estrutura do projeto e iniciado a pagina de login

--- pipoca-agil/src/App.jsx
+++ pipoca-agil/src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './HomePage'
-import './css/index.css'
-import './css/home.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-)
+function App(){
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />        
+      </Routes>
+    </Router>
+    );
+  };
+  
+  export default App;
+ 
---
+++ pipoca-agil/src/pages/Login/index.jsx
@@ -0,0 +1,22 @@
+import { Header } from '../../components/Header'
+import './login.css'
+
+export function Login() {
+  return (
+    <>
+      <Header />
+      <div className="login-container">
+
+        <div className='login-content'>
+          <div className='ibson-face'>
+            <img src="/assets/png/ibson-face.png" alt="" />
+          </div>
+
+          <div className='login-content-main'>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
